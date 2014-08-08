d664ae98f1249a86bd0f8ebcfb659c8925bfb479
Glenn Plas <glenn@byte-consult.be>
2014-08-08 15:24:33 +0200

    add lang files

---
+++ app/lang/nl/reminders.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "Paswoord moet minstens 6 lang zijn.",
+
+	"user"     => "Geen user met deze email gevonden.",
+
+	"token"    => "Dit paswoord reset token is niet geldig.",
+
+	"sent"     => "Paswoord herinnering gezonden!",
+
+);
--- app/routes.php
+++ app/routes.php
@@ -11,8 +11,23 @@
 |
 */
 
-Route::get('/', 'HomeController@getIndex');
-Route::get('download', 'HomeController@getDownload');
+$languages = array('nl','fr');
+$locale = Request::segment(1);
+if(in_array($locale, $languages)){
+   \App::setLocale($locale);
+} else {
+   $locale = null;
+}
 
-Route::post('form',  array('before' => 'csrf','uses'=>'HomeController@postIndex'));
-Route::post('upload', 'HomeController@postUpload');
+Route::group(array('prefix' => $locale), function()
+      {
+      //Route::get('/', array('as' => 'home', function(){ //Do something  }));
+      //Route::get('news', array('as' => 'news', 'uses' => 'NewsController@showIndex'));
+      //Route::get('{slug}', 'PageController@showPage');
+
+   Route::get('/', 'HomeController@getIndex');
+   Route::get('download', 'HomeController@getDownload');
+
+   Route::post('form',  array('before' => 'csrf','uses'=>'HomeController@postIndex'));
+   Route::post('upload', 'HomeController@postUpload');
+});
