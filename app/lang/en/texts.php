2cb2b8a972f42a209ba26aba47de35ee500e0362
Glenn Plas <glenn@byte-consult.be>
2014-08-08 22:39:42 +0200

    custom translations

---
+++ app/lang/en/texts.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| custom translation
+	|--------------------------------------------------------------------------
+	|
+	*/
+
+	"tips" => "Where tha dog go.",
+	"busystreets" => "High-traffic streets",
+   "libraries" => "Libraries",
+   "coffeshops" => "Coffee Shops",
+   "gasstations" => "Gas Stations",
+   "drugstores" => "Drug Stores",
+   "schools" => "Schools"
+);
---
+++ app/lang/nl/texts.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| custom translation
+	|--------------------------------------------------------------------------
+	|
+	*/
+
+	"tips" => "Waar ge awe hond moe vinne!.",
+	"busystreets" => "Drukke straten",
+   "libraries" => "Bibliotheken",
+   "coffeshops" => "Taverne/Cafe",
+   "gasstations" => "Tankstations",
+   "drugstores" => "Apothekers",
+   "schools" => "Scholen"
+);
