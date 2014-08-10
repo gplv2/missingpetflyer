17bb7c40372e3a88b024a68625f5da3093b35d01
Glenn Plas <glenn@byte-consult.be>
2014-08-10 17:52:17 +0200

    patch annoying z-index behavior between UI and openlayers map

--- public/js/main.js
+++ public/js/main.js
@@ -6,8 +6,24 @@ $(document).ready(function () {
 
    $('#radio').buttonset();
    $('#date').datepicker({ dateFormat:"yy-mm-dd" });
-   $('#track').button();
+/*
+   $( ".ui-datepicker" ).on( "mouseover", function() {
+     $( this ).zIndex(99999);
+   });
 
+   $( "#ui-datepicker-div" ).hover(function() {
+   	$('#ui-datepicker-div').zIndex(99999);
+   });
+*/
+
+   $("#date").on( "click", function() {
+   	$('#ui-datepicker-div').zIndex(99999);
+   });
+
+   // $('.ui-datepicker').css("z-index", 99999);
+   // $.datepicker.setDefaults( $.datepicker.regional[ "fr" ] );
+   $('#track').button();
    // $( "input[type=submit], a, button" ).button().click(function( event ) { event.preventDefault(); });
    $( '#locate' ).button().click(function( event ) { event.preventDefault(); });
+
 });
